a7135ff13e06ee527b13db1a16e66dc3fdcfca1a
Taisen.fr (Dev) <vaca@hotmail.fr>
2022-08-10 08:41:48 +0200

    2.0.8

---
+++ Classes/SimpleClasses.cs
@@ -0,0 +1,35 @@
+﻿using Blazored.LocalStorage;
+
+namespace TAISEN.Classes
+{
+
+    public class SEtape
+    {
+        public string Etape { get; set; }
+        public string Objectif { get; set; }
+        public IEnumerable<Atelier> Ateliers { get; set; }
+    }
+
+    public class Atelier
+    {
+        public string Nom { get; set; }
+        public string? Prerequis { get; set; }
+        public int Poids { get; set; }
+        public IEnumerable<Processus> Processus { get; set; }
+        public IEnumerable<Livrables> Livrables { get; set; }
+    }
+
+    public class Processus
+    {
+        public string Nom { get; set; }
+        public bool Fin { get; set; }
+    }
+
+    public class Livrables
+    {
+        public string Nom { get; set; }
+        public string? CR { get; set; }
+    }
+
+
+}
