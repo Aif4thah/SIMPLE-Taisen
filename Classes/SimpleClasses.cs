7dba14c6ba70bd02cbcebf3f8ad3dc009584917d
Taisen.fr (Dev) <vaca@hotmail.fr>
2022-09-06 16:00:11 +0200

    2.3.8

--- Classes/SimpleClasses.cs
+++ Classes/SimpleClasses.cs
@@ -15,20 +15,18 @@ namespace TAISEN.Classes
         public string Nom { get; set; }
         public string Description { get; set; }
         public string? Prerequis { get; set; }
-        public List<string>? Interlocuteurs {get; set;}
+        public List<string>? Interlocuteurs { get; set; }
         public int Poids { get; set; }
         public IEnumerable<Processus> Processus { get; set; }
-        
     }
 
     public class Processus
     {
         public string Nom { get; set; }
         public Referentiel? Referentiel { get; set; }
-        public IEnumerable<Outil>? Outils {get;set;}
-        public IEnumerable<Livrable>? Livrables { get; set; }
+        public IEnumerable<Outil>? Outils { get; set; }
+        public IEnumerable<Livrable> Livrables { get; set; }
         public bool Fin { get; set; }
-
     }
 
     public class Livrable
@@ -39,8 +37,8 @@ namespace TAISEN.Classes
 
     public class Referentiel
     {
-        public string Nom {get; set;}
-        public string? Uri {get;set;}
+        public string Nom { get; set; }
+        public string? Uri { get; set; }
     }
 
     public class Outil
@@ -49,29 +47,41 @@ namespace TAISEN.Classes
         public string? Uri { get; set; }
     }
 
-    public class CompteRendu {
+    public class CompteRendu
+    {
         public string? Texte { get; set; }
         public List<El>? Els { get; set; }
-
-        public List<FonctionSensible>? FonctionSensibles { get;set;}
+        public List<FonctionSensible>? FonctionSensibles { get; set; }
+        public List<MesureSecurite>? MesureSecurites { get; set; }
     }
 
     public class El
     {
-        public string? Texte {get;set;}
-              
+        public string? Texte { get; set; }
     }
 
     public class FonctionSensible
     {
-        public string? Nom { get; set; }
-        public bool Spoofing { get;set;}
-        public bool Tampering { get;set;}
-        public bool Repudiation { get;set;}   
-        public bool InformationDisclosure { get;set;}
+        public string Nom { get; set; }
+        public bool Spoofing { get; set; }
+        public bool Tampering { get; set; }
+        public bool Repudiation { get; set; }
+        public bool InformationDisclosure { get; set; }
         public bool DenialOfService { get; set; }
-        public bool ElevationOfPrivilege { get;set;}
+        public bool ElevationOfPrivilege { get; set; }
+    }
 
+    public class MesureSecurite
+    {
+        public string? FonctionCible { get; set; }
+        public string? Nom { get; set; }
+        public string? Description { get; set; }
+        public bool SpoofingPrevention { get; set; }
+        public bool TamperingPrevention { get; set; }
+        public bool RepudiationPrevention { get; set; }
+        public bool InformationDisclosurePrevention { get; set; }
+        public bool DenialOfServicePrevention { get; set; }
+        public bool ElevationOfPrivilegePrevention { get; set; }
     }
 
 
